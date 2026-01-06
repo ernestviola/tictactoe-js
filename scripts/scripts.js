c88a181d8eb0ff4a41b589261d83ef816ffcf156
Ernest V <ernest.viol@gmail.com>
2026-01-05 16:39:56 -0800

    Complete game logic
    Completed the game logic with a single javascript entry. Makes use of
    the factory function and module pattern.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,3 @@
-// create a gameboard
-// setNewGameBoardState
-// check if gameBoard is in a final state -> won or no moves available
-// printBoard
-
-// set player name and token and numberOfWins
-
-// start the game
-// player turn -> player turn next
-// check board for game state end -> if winner increment player wins
-
-
 function boardController() {
   const rows = 3;
   const columns = 3
@@ -44,36 +32,95 @@ function boardController() {
     // check if the winning condition is found if so then return the winner token
     return {
       full: isBoardFull(gameBoard),
-      winner: checkWinner()
+      winner: hasWinner()
     }
 
     function isBoardFull(arr) {
-      for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-          return isBoardFull(arr[i]);
-        } else {
-          return i==arr.length -1 && arr[i] !== null; 
-        }
-      }
+      return arr.flat(Infinity).every(e => e !== null);
     }
 
-    function checkWinner() {
+    function hasWinner() {
+      return checkRows() || checkColumns() || checkDiagonals();
+
       // check rows
-      for (let i = 0; i < gameBoard.length; i++) {
+      function checkRows() {
+        for (let i = 0; i < gameBoard.length; i++) {
         const currentRow = gameBoard[i];
-        if (!currentRow[i]) {
+        if (!currentRow[0]) {
+          // if first index of current row is empty then not a row win
           continue;
         }
         const currentSymbol = currentRow[i];
         if (currentRow.every(e => e === currentSymbol)) {
+          console.log('Row Win');
           return true;
         }
       }
-      // check columns
-      for (let i = 0; i < gameBoard.length; i++) {
-
       }
+
+      // check columns
+      function checkColumns() {
+        for (let r = 0; r < rows; r++) {
+        let columnWin = false;
+        let currentSymbol;
+        for (let c = 0; c < columns; c++) {
+          
+          if (c == 0 && gameBoard[0][r]) {
+            currentSymbol = gameBoard[0][r];
+          }
+          if (!currentSymbol) break;
+
+          if (currentSymbol == gameBoard[c][r]) {
+            columnWin = true;
+          } else {
+            columnWin = false;
+            break;
+          }
+        }
+        if (columnWin) {
+          console.log('Column Win')
+          return columnWin;
+        }
+      }
+      }
+
       // check diagonals
+      function checkDiagonals() {
+        return checkTopLeftToBottomRight() || checkBottomLeftToTopRight();
+        function checkTopLeftToBottomRight() {
+          let isWinner = false;
+          let currRow = 0;
+          let currCol = 0;
+          const currentSymbol = gameBoard[currRow][currCol];
+          while (currRow < rows && currCol < columns) {
+            if (currentSymbol && gameBoard[currRow][currCol] === currentSymbol) {
+              isWinner = true;
+            } else {
+              return false;
+            }
+            currRow++;
+            currCol++;
+          }
+          return isWinner;
+        }
+        function checkBottomLeftToTopRight() {
+          let isWinner = false;
+          let currRow = 0;
+          let currCol = columns-1;
+          const currentSymbol = gameBoard[currRow][currCol];
+          while (currRow < rows && currCol >=0) {
+            if (currentSymbol && gameBoard[currRow][currCol] === currentSymbol) {
+              isWinner = true;
+            } else {
+              return false;
+            }
+            currRow++;
+            currCol--;
+          }
+          return isWinner;
+        }
+      }
+      
     }
   }
 
@@ -86,16 +133,19 @@ function playerController() {
 
   const newPlayer = (name, symbol) => {
     // check to see if name or symbol already exists if it does then redo
+    let wins = 0;
+    const increaseScore = () => wins++;
     if (players.find(player => player.name == name)) {
       throw Error('Name is already in use');
     } else if ((players.find(player => player.symbol == symbol))) {
-      throw Error('Name is already in use');
+      throw Error('Symbol is already in use');
     }
     players.push(
       {
         name,
         symbol,
-        wins: 0
+        wins,
+        increaseScore
       }
     );
   }
@@ -111,7 +161,8 @@ function playerController() {
 
 
 function gameController() {
-  let board = boardController();
+  let board = null;
+  let winner = null;
   const players = playerController();
 
   const startGame = () => {
@@ -121,34 +172,33 @@ function gameController() {
   }
 
   const playRound = (row, column) => {
+    if (board.checkBoardState().full) {
+      console.log('Unable to continue. Start a new game.');
+    }
+    if(winner) {
+      console.log('Play a new game current winner is:', winner.name)
+    }
     if (board.playerTurn(row, column, players.getCurrentPlayer())) {
-      players.nextPlayer();
       board.printBoard();
+      const currentBoardState = board.checkBoardState();
 
-      if (board.checkBoardState().full) {
-        console.log('Full')
-      }
+      if (currentBoardState.full) {
+        console.log('Unable to continue. Start a new game.');
+      } else if (currentBoardState.winner) {
+        winner = players.getCurrentPlayer();
+        winner.increaseScore();
+        console.log('Play a new game current winner is:', winner.name);
+        console.log(`${winner.name} has a score of ${winner.wins}`);
+      } else {
+        players.nextPlayer();
+      }        
     }
   }
 
   players.newPlayer('player 1', 'X');
   players.newPlayer('player 2', 'O');
 
-  startGame();
-
   return { startGame,playRound }
 }
 
 const gc = gameController();
-
-gc.playRound(0,0);
-gc.playRound(0,1);
-gc.playRound(0,2);
-
-gc.playRound(1,0);
-gc.playRound(1,1);
-gc.playRound(1,2);
-
-gc.playRound(2,0);
-gc.playRound(2,1);
-gc.playRound(2,2);
